2daf3655dedb2011b5bb0b9233d3e39228f34281
Stefan <stefan@cyphoria.net>
2013-03-19 21:41:06 +0100

    Initial commit for play app

---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+
+object ApplicationBuild extends Build {
+
+  val appName         = "WeddingApp"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
