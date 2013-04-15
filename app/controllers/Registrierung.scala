cf535aa195ddd1e63d116d8a25a8f3b966968cae
Stefan <stefan@cyphoria.net>
2013-04-15 15:48:49 +0200

    formular inhalte und validierung implementiert/konfiguriert

--- app/controllers/Registrierung.scala
+++ app/controllers/Registrierung.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.data.validation.Constraints._
 import registrierung.HochzeitsGastBewerbung
 
 /**
@@ -13,12 +14,22 @@ object Registrierung extends Controller {
 
   val registrierungsFormular = Form(
     mapping(
-        "nutzername" -> text(minLength = 3, maxLength = 50),
-        "email" -> email
-    )(HochzeitsGastBewerbung.apply)(HochzeitsGastBewerbung.unapply)
+        "nutzername" -> text(minLength = 3, maxLength = 50)
+                        .verifying(pattern("""[a-zA-Z0-9]*""".r, "nutzername.pattern")),
+        "email" -> email,
+        "sicherheitsfrage" -> text(maxLength=150)
+                              .verifying("sicherheitsfrage.error", antwort => antwort.toLowerCase == "albert schweizer")
+    )((name, email, _) => HochzeitsGastBewerbung(name, email))
+     ((bewerbung: HochzeitsGastBewerbung) => Some(bewerbung.nutzername, bewerbung.email, ""))
   )
 
-  def registrieren = TODO
+  def registrieren = Action { implicit request =>
+    registrierungsFormular.bindFromRequest.fold(
+      errors => BadRequest(views.html.registrieren(errors)),
+
+      bewerbung => Ok(views.html.registrieren(registrierungsFormular))
+    )
+  }
 
   def formularAnzeigen = Action {
     Ok(views.html.registrieren(registrierungsFormular))
