891927470dd72dd0280099ba773b49876c7ddd90
Stefan <stefan@cyphoria.net>
2013-05-13 22:30:41 +0200

    Gästeliste hinzugefügt: Fehlte weil git nicht mit ä im Dateinamen klar kam

---
+++ app/model/Gaesteliste.scala
@@ -0,0 +1,70 @@
+package model
+
+import play.api.db.DB
+import anorm._
+import play.api.Play.current
+
+/**
+ *
+ * @author Stefan Penndorf <stefan@cyphoria.net>
+ */
+trait Gästeliste {
+  def gäste: List[Benutzer]
+  def findeGastMitName(name: BenutzerName): Option[Benutzer]
+  def findeGastMitId(id: Long): Option[Benutzer]
+  def findeGastMitEMail(email: EMail): Option[Benutzer]
+}
+
+
+class PersistenteGästeliste extends Gästeliste {
+
+  def findeGastMitName(name: BenutzerName) =  {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT * FROM users WHERE vorname={vorname} AND nachname={nachname}
+        """
+      ).on(
+        'vorname -> name.vorname,
+        'nachname -> name.nachname
+      ).as(Benutzer.simple.singleOpt)
+    }
+  }
+
+
+  def findeGastMitId(id: Long) = {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT * FROM users WHERE id={id}
+        """
+      ).on(
+        'id -> id
+      ).as(Benutzer.simple.singleOpt)
+    }
+  }
+
+
+  def gäste = {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT * FROM users
+        """
+      ).as(Benutzer.simple *)
+    }
+  }
+
+  def findeGastMitEMail(email: EMail) = {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT * FROM users WHERE email={email}
+        """
+      ).on(
+        'email -> email.email
+      ).as(Benutzer.simple.singleOpt)
+    }
+  }
+
+}
