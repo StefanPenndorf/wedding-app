3e1be0b94ee3c5e805d20df90061a9c83f4210dd
Stefan <stefan@cyphoria.net>
2013-08-03 13:48:19 +0200

    Aufgeräumt: Logging reduziert (debugausgaben werden nicht mehr benötigt)

--- test/net/cyphoria/weddingapp/specification/infrastructure/Infrastructure.scala
+++ test/net/cyphoria/weddingapp/specification/infrastructure/Infrastructure.scala
@@ -3,7 +3,6 @@ package net.cyphoria.weddingapp.specification.infrastructure
 import cucumber.api.scala.ScalaDsl
 import cucumber.api.Scenario
 import com.icegreen.greenmail.util.{ServerSetupTest, GreenMail}
-import org.slf4j.LoggerFactory
 
 /**
  *
@@ -11,21 +10,15 @@ import org.slf4j.LoggerFactory
  */
 class Infrastructure extends ScalaDsl with HookRegistering {
 
-  val logger = LoggerFactory.getLogger("test-infras")
-
   def mailer = global[GreenMail]("mailServer")
 
   Before { f: (Scenario) => Unit
-    logger.error("Starting mailer2..." + Thread.currentThread().toString)
     val mailer: GreenMail = new GreenMail(ServerSetupTest.SMTP)
     mailer.start()
-    logger.error("... started" + Thread.currentThread().toString)
     registerGlobal("mailServer", mailer)
   }
 
   After { f: (Scenario) => Unit
-    logger.error("Stopping mailer 2"  + Thread.currentThread().toString)
-
     global[GreenMail]("mailServer").stop()
     unregisterGlobal("mailServer")
   }
